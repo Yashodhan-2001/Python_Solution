b3139b2c2d424435000031b7813b86eb23ead5d3
Yashodhan2001 <89022164+Yashodhan2001@users.noreply.github.com>
2022-05-23 11:54:26 +0530

    This is iterator commit

---
+++ iterators.py
@@ -0,0 +1,11 @@
+strv="Yashodhan"
+ite=iter(strv)
+print(ite.__next__())#Y
+print(ite.__next__())#a
+print(ite.__next__())#s
+print(ite.__next__())#h
+print(ite.__next__())#o
+print(ite.__next__())#d
+print(ite.__next__())#h
+print(ite.__next__())#a
+print(ite.__next__())#n
