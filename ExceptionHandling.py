203d8fae78e437e12bdbe024ad275d140a1db544
Yashodhan2001 <89022164+Yashodhan2001@users.noreply.github.com>
2022-05-24 22:48:48 +0530

    Added two more file here

---
+++ ExceptionHandling.py
@@ -0,0 +1,19 @@
+
+
+
+# def div(n):
+#     h=n/0
+#     print(b)
+# try:
+#     div(3)  
+#     div(5)
+# except ZeroDivisionError:
+#     print("ZeroDivisiorError Occured and handled")  
+# except NameError:    
+ #      print("Name error occured and handled")
+try:
+    k=5//0
+except ZeroDivisionError:
+    print("Zero Division Error and handled")
+finally:
+    print("This has to be execute")              
---
+++ snippet.py
@@ -0,0 +1,19 @@
+# x = ['ab', 'cd']
+# for i in x:
+#     i.upper()
+# print(x)
+
+# i = 2
+# while True:
+#     if i%3 == 0:
+#         break
+#     print(i)
+#     i += 2
+    
+d = {0: 'a', 1: 'b', 2: 'c'}
+for i in d.keys():
+     print(d[i])  
+
+# d = {0, 1, 2}
+# for x in d:
+#     print(x)  
