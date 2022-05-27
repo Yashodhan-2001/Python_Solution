a81fd651c174f3a8fc12b492743273a4590b842e
Yashodhan2001 <89022164+Yashodhan2001@users.noreply.github.com>
2022-05-27 23:19:20 +0530

    This is one more commit for regex

--- regex.py
+++ regex.py
@@ -13,8 +13,52 @@
 # print(match.start())
 # print(match.end())
 
+# Character metadat uses
+# import re
+# matches="Yes this computer is very expensive and tis computer have lotrs of fetures and apps also this computer is solid for playing any type of games"
+# result=re.findall(r'[Cc]omputer',matches)
+# print(result)
+
+# import re 
+# text="It is me and here It my hood no one Will be there"
+
+# result=re.findall(r'there$',text)
+# if result:
+#     print("YEs i am here")
+# else:
+#     print("Acha chlta hu duio mai yaad rkhna")    
+
+# import re
+
+# text ="Yash is a programmer"
+# result=re.findall(r'Y...h',text)
+# print(result)
+
+# import re
+# txt=" helo hello yash"
+# result=re.findall(r'he.+o',txt)
+# print(result)
+
+# import re
+# txt=" ho"
+# result=re.findall(r'h.+o',txt)
+# print(result)
+
+# import re
+# txt="Yash hello"
+# result=re.findall(r"he.?o",txt)
+# print(result)
+
+# import re
+# text="hello yash"
+# result=re.findall(r"he.{2}o",text)
+# print(result)
 
 import re
-matches="Yes this computer is very expensive and tis computer have lotrs of fetures and apps also this computer is solid for playing any type of games"
-result=re.findall(r'[Cc]omputer',matches)
+txt="This is yash from up uttar pradesh"
+result=re.findall(r"mzn|yash",txt)
 print(result)
+if result:
+    print("Yes got 1 string from the container")
+else:
+    print("No bad luch please next time")    
